887bdac950b37d8244e0095cf18bcbed992aa513
Nolan <ardolino.nolan@gmail.com>
2020-05-08 14:52:23 -0400

    updating all modified files

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -5,6 +5,8 @@ A python package for analyzing and visualizing pdb and xyz files. for MolSSI May
 
 # Add imports here
 from .functions import *
+#from .functions import canvas
+#import molecool.functions # changes external function call (molecool.functions.canvas())
 
 # Handle versioneer
 from ._version import get_versions
